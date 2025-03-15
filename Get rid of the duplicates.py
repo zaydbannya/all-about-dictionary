8c148305bbcc6f03b5024a66263c3722c9eda594
zaydbannya <zayd.bannya@gmail.com>
2025-03-15 15:38:09 +0000

    get rid of duplication

---
+++ Get rid of the duplicates.py
@@ -0,0 +1,26 @@
+stun_data={'id1':
+    {'name':['Sara'],
+     'class':['V'],
+     'subject-integration':['english,math,science']
+},
+'id2':
+    {'name':['David'],
+     'class':['V'],
+     'subject-integration':['english,math,science']
+},
+'id3':
+    {'name':['Sara'],
+     'class':['V'],
+     'subject-integration':['english,math,science']
+},
+'id4':
+    {'name':['Surya'],
+     'class':['V'],
+     'subject-integration':['english,math,science']
+},
+}
+result={}
+for key, value in stun_data.items():
+    if value not in result.values():
+        result[key]=value
+print(result)
